15d04724101b161e50a1172bb2f5c313d3285e6c
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-17 18:12:06 +0300

    Add small performance test

--- Sources/ThGPX/GPX.swift
+++ Sources/ThGPX/GPX.swift
@@ -1,13 +1,15 @@
 import SWXMLHash
 
+fileprivate let creatorThGPX = "ThGPX (https://github.com/nikstar/ThGPX)"
+
 /// GPX documents contain a metadata header, followed by waypoints, routes, and tracks.  You can add your own elements to the extensions section of the GPX document.
 public struct GPX {
     
     /// You must include the version number in your GPX document.
-//    public let version = "1.1"
+    public let version = "1.1"
     
     /// You must include the name or URL of the software that created your GPX document.  This allows others to inform the creator of a GPX instance document that fails to validate.
-//    public var creator = "ThGPX (https://github.com/nikstar/ThGPX)"
+    public var creator = creatorThGPX
     
     /// Metadata about the file.
     public var metadata: Metadata?
@@ -28,10 +30,12 @@ public struct GPX {
 extension GPX: XMLIndexerDeserializable {
     public static func deserialize(_ node: XMLIndexer) throws -> GPX {
         GPX(
+            creator: node.value(ofAttribute: "creator") ?? creatorThGPX,
             metadata: try node["metadata"].value(),
-            waypoints: try node["wpt"].all.map { try $0.value() },
-            routes: try node["rte"].all.map { try $0.value() },
-            tracks: try node["trk"].all.map { try $0.value() }
+            waypoints: try node["wpt"].all.map(Waypoint.deserialize),
+            routes: try node["rte"].all.map(Route.deserialize),
+            tracks: try node["trk"].all.map(Track.deserialize)
         )
     }
 }
+
