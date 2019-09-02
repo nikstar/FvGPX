ee2ffe56c9dc6f44af9f650c70aac9ad3efebb47
Nikita Starshinov <nstarshinov@yandex.ru>
2019-09-03 02:04:13 +0300

    Fix Linux tests

--- Tests/GPXTests/XCTestManifests.swift
+++ Tests/GPXTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(ThGPXTests.allTests),
+        testCase(GPXTests.allTests),
     ]
 }
 #endif
